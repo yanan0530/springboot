7f587077a9ca1e8401f7f3c2efaa7472bf279c9b
Yanan <550985993@qq.com>
2019-04-23 16:35:49 +0800

    添加事务

--- src/main/java/com/example/datajdbc/service/impl/RecordsServiceImpl.java
+++ src/main/java/com/example/datajdbc/service/impl/RecordsServiceImpl.java
@@ -5,10 +5,13 @@ import com.example.datajdbc.pojo.Records;
 import com.example.datajdbc.service.RecordsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.beans.Transient;
 import java.util.List;
 
 @Service
+@Transactional
 public class RecordsServiceImpl implements RecordsService {
     @Autowired
     private RecordsMapper recordsMapper;
